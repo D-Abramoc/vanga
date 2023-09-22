5c0a2bb0f5ec9896f2d41f507576df1623fab5aa
Dmitry Abramov <elektrik.kem@yandex.ru>
2023-09-23 01:29:16 +0700

    create api, vanga

--- vanga_back/vanga_back/settings.py
+++ vanga_back/vanga_back/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w8x1hoag*7$ljcuhb-39c0=w4^foa$yt2c_8ca)g+)*-7+_g_='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'vanga.apps.VangaConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
