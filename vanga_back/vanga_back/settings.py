2a4a8b9f5bfb3f18a87d23c992903189eec2995c
Дмитрий Абрамов <elektrik.kem@yandex.ru>
2023-09-23 15:10:32 +0700

    fix: env.sample, settings, requirements

--- vanga_back/vanga_back/settings.py
+++ vanga_back/vanga_back/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',
+                 'localhost']
 
 
 # Application definition
@@ -83,8 +84,12 @@ WSGI_APPLICATION = 'vanga_back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
