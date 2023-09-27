7b8fc8a9d2fc2d2274ec2f19a80d82b73c2d0211
Dmitry Abramov <elektrik.kem@yandex.ru>
2023-09-27 19:45:41 +0700

    add: serializers

--- vanga_back/api/urls.py
+++ vanga_back/api/urls.py
@@ -1,19 +1,28 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import CityViewSet, ProductViewSet, ShopViewSet
+from .views import (CityViewSet, ProductViewSet, ShopViewSet, ForecastViewSet,
+                    SaleViewSet, CategoryViewSet, GroupViewSet,
+                    DivisionViewSet)
 from .custom_routers import OnlyGetRouter
 
 
 app_name = 'api'
 
-router_v1 = OnlyGetRouter()
+router_v1 = routers.DefaultRouter()
+router_v1_only_get = OnlyGetRouter()
 
-router_v1.register(r'shops', ShopViewSet, basename='shops')
-router_v1.register(r'categories', ProductViewSet, basename='categories')
-router_v1.register(r'cities', CityViewSet, basename='cities')
+router_v1.register(r'forecasts', ForecastViewSet, basename='forecasts')
+router_v1.register('sales', SaleViewSet, basename='sales')
+router_v1_only_get.register(r'shops', ShopViewSet, basename='shops')
+router_v1_only_get.register(r'products', ProductViewSet, basename='products')
+router_v1_only_get.register(r'cities', CityViewSet, basename='cities')
+router_v1_only_get.register(r'categories', CategoryViewSet, basename='categories')
+router_v1_only_get.register(r'groups', GroupViewSet, basename='groups')
+router_v1_only_get.register(r'divisions', DivisionViewSet, basename='divisions')
 
 urlpatterns = [
-    path('v1/', include(router_v1.urls)),
+    path('v1/', include(router_v1_only_get.urls)),
+    path('v1/', include(router_v1.urls))
 ]
 
--- vanga_back/api/views.py
+++ vanga_back/api/views.py
@@ -1,9 +1,68 @@
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import viewsets
 
-from backend.models import City, Product, Shop
+from backend.models import (City, Product, Shop, Forecast, Sale, Category,
+                            Group, Division)
 
-from .serializers import CitySerializer, ProductSerializer, ShopSerializer
+from .serializers import (CitySerializer, ProductSerializer, ShopSerializer,
+                          ForecastSerializer, SaleSerializer,
+                          CategorySerializer, GroupSerializer,
+                          DivisionSerializer)
+
+
+@extend_schema(tags=['Прогноз'])
+@extend_schema_view(
+    list=extend_schema(
+        summary='Получить прогноз'
+    )
+)
+class ForecastViewSet(viewsets.ModelViewSet):
+    queryset = Forecast.objects.all()
+    serializer_class = ForecastSerializer
+
+
+@extend_schema(tags=['Продажи'])
+@extend_schema_view(
+    list=extend_schema(
+        summary='Получить список продаж'
+    )
+)
+class SaleViewSet(viewsets.ModelViewSet):
+    queryset = Sale.objects.all()
+    serializer_class = SaleSerializer
+
+
+@extend_schema(tags=['Категории'])
+@extend_schema_view(
+    list=extend_schema(
+        summary='Получить список категорий'
+    )
+)
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+@extend_schema(tags=['Группы товаров'])
+@extend_schema_view(
+    list=extend_schema(
+        summary='Получить группы товаров'
+    )
+)
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+@extend_schema(tags=['Дивизионы'])
+@extend_schema_view(
+    list=extend_schema(
+        summary='Получить список дивизионов'
+    )
+)
+class DivisionViewSet(viewsets.ModelViewSet):
+    queryset = Division.objects.all()
+    serializer_class = DivisionSerializer
 
 
 @extend_schema(tags=['Список ТЦ'])
