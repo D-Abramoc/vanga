ecab0c0f64a365f9b61fe0110817c8a70c8b182b
unknown <andrlecht@yandex.ru>
2023-09-27 03:21:03 +0300

    finish import + fix models

---
+++ vanga_back/backend/management/commands/clear_db.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+
+from backend import models as m
+
+
+class Command(BaseCommand):
+    help = 'Удаление всех объектов из БД'
+
+    def handle(self, *args, **options):
+        m.City.objects.all().delete()
+        m.Division.objects.all().delete()
+        m.Shop.objects.all().delete()
+        m.Group.objects.all().delete()
+        m.Category.objects.all().delete()
+        m.Subcategory.objects.all().delete()
+        m.Product.objects.all().delete()
+        m.Sale.objects.all().delete()
+        print('База очищена')
