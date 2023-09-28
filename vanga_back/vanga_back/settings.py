ee83106a6b8988a94f3887c6d3f24c4fc903db06
Dmitry Abramov <elektrik.kem@yandex.ru>
2023-09-28 21:56:17 +0700

    add: config for flake8

--- vanga_back/vanga_back/settings.py
+++ vanga_back/vanga_back/settings.py
@@ -117,7 +117,7 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
