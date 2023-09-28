d2c148b1d730c60d32df1921dafeea135dbb5d6d
Dmitry Abramov <elektrik.kem@yandex.ru>
2023-09-28 21:45:42 +0700

    fix: flake8 api.serializers

--- vanga_back/api/serializers.py
+++ vanga_back/api/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
-
-from backend.models import (City, Division, Group, Category, Sale, Forecast,
-                            Product, Shop, Subcategory)
+from backend.models import (Category, City, Division, Forecast, Group,
+                            Product, Sale, Shop, Subcategory)
 
 
 class ForecastSerializer(serializers.ModelSerializer):
@@ -14,6 +13,7 @@ class SaleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sale
         fields = '__all__'
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -36,7 +36,9 @@ class DivisionSerializer(serializers.ModelSerializer):
 class ShopSerializer(serializers.ModelSerializer):
     store = serializers.CharField(source='st_id')
     city = serializers.CharField(source='st_city_id.city_id')
-    division = serializers.CharField(source='st_division_code_id.division_code_id')
+    division = serializers.CharField(
+        source='st_division_code_id.division_code_id'
+    )
     type_format = serializers.IntegerField(source='st_type_format_id')
     loc = serializers.IntegerField(source='st_type_loc_id')
     size = serializers.IntegerField(source='st_type_size_id')
@@ -57,7 +59,9 @@ class SubcategorySerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     sku = serializers.CharField(source='pr_sku_id')
-    group = serializers.CharField(source='pr_subcat_id.cat_id.group_id.group_id')
+    group = serializers.CharField(
+        source='pr_subcat_id.cat_id.group_id.group_id'
+    )
     category = serializers.CharField(source='pr_subcat_id.cat_id.cat_id')
     subcategory = serializers.CharField(source='pr_subcat_id.subcat_id')
     uom = serializers.IntegerField(source='pr_uom_id')
