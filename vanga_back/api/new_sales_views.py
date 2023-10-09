afa97c64941dafa38df36f746c9fac7e8934b4e4
Dmitry Abramov <elektrik.kem@yandex.ru>
2023-10-09 19:39:59 +0700

    aaa

--- vanga_back/api/new_sales_views.py
+++ vanga_back/api/new_sales_views.py
@@ -8,7 +8,7 @@ from backend.models import Sale
 class SaleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sale
-        fields = ('date', 'pr_sku_id')
+        fields = ('date', 'pr_sales_in_units')
 
 
 @extend_schema_view(
@@ -18,30 +18,30 @@ class SaleSerializer(serializers.ModelSerializer):
             'store', OpenApiTypes.INT, OpenApiParameter.QUERY,
             required=True
         ),
-        OpenApiParameter(
-            'group', OpenApiTypes.INT, OpenApiParameter.QUERY,
-            required=True
-        ),
-        OpenApiParameter(
-            'category', OpenApiTypes.INT, OpenApiParameter.QUERY,
-            required=True
-        ),
-        OpenApiParameter(
-            'subcategory', OpenApiTypes.INT, OpenApiParameter.QUERY,
-            required=True
-        ),
+        # OpenApiParameter(
+        #     'group', OpenApiTypes.INT, OpenApiParameter.QUERY,
+        #     required=True
+        # ),
+        # OpenApiParameter(
+        #     'category', OpenApiTypes.INT, OpenApiParameter.QUERY,
+        #     required=True
+        # ),
+        # OpenApiParameter(
+        #     'subcategory', OpenApiTypes.INT, OpenApiParameter.QUERY,
+        #     required=True
+        # ),
         OpenApiParameter(
             'sku', OpenApiTypes.INT, OpenApiParameter.QUERY,
             required=True
         ),
-        OpenApiParameter(
-            'start', OpenApiTypes.DATETIME, OpenApiParameter.QUERY,
-            required=True
-        ),
-        OpenApiParameter(
-            'end', OpenApiTypes.DATETIME, OpenApiParameter.QUERY,
-            required=True
-        )
+        # OpenApiParameter(
+        #     'start', OpenApiTypes.DATETIME, OpenApiParameter.QUERY,
+        #     required=True
+        # ),
+        # OpenApiParameter(
+        #     'end', OpenApiTypes.DATETIME, OpenApiParameter.QUERY,
+        #     required=True
+        # )
         ]
     )
 )
@@ -54,13 +54,13 @@ class NewSalesViewSet(viewsets.ModelViewSet):
             Sale.objects.filter(
                 st_id=self.request.query_params['store'],
                 pr_sales_type_id=False,
-                pr_sku_id__pr_subcat_id__cat_id__group_id=self.request.query_params['group'],
-                pr_sku_id__pr_subcat_id__cat_id=self.request.query_params['category'],
-                pr_sku_id__pr_subcat_id=self.request.query_params['subcategory'],
+                # pr_sku_id__pr_subcat_id__cat_id__group_id=self.request.query_params['group'],
+                # pr_sku_id__pr_subcat_id__cat_id=self.request.query_params['category'],
+                # pr_sku_id__pr_subcat_id=self.request.query_params['subcategory'],
                 pr_sku_id=self.request.query_params['sku'],
-                date__range=[
-                    self.request.query_params['start'],
-                    self.request.query_params['end']
-                ]
+                # date__range=[
+                #     self.request.query_params['start'],
+                #     self.request.query_params['end']
+                # ]
             )
         )
