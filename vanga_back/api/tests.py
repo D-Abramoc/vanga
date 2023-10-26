49bbb406e1ea39936e20ce41e50df9d177bfb22e
Дмитрий Абрамов <elektrik.kem@yandex.ru>
2023-10-26 17:18:23 +0700

    Переделал часть тестов

--- vanga_back/api/tests.py
+++ vanga_back/api/tests.py
@@ -1,9 +1,10 @@
 from http import HTTPStatus
 
+from django.shortcuts import get_object_or_404
 from rest_framework.test import APIClient, APITestCase
 
 from backend.management.commands import import_db, import_sales_fake
-from backend.models import Product, Shop, City, Division, Group, Category
+from backend.models import Product, Shop, City, Division, Group, Category, Subcategory, Sale
 from forecast.management.commands import import_fc_fake
 from users.models import User
 
@@ -22,30 +23,106 @@ class ApiTest(APITestCase):
     def setUpClass(cls) -> None:
         super().setUpClass()
         City.objects.create(
-            city_id='Moskow'
+            city_id='Moscow'
         )
         City.objects.create(
             city_id='Kemerovo'
         )
         Division.objects.create(
-            division_code_id='first-division'
+            division_code_id='first_division'
         )
         Division.objects.create(
-            division_code_id='second-division'
+            division_code_id='second_division'
         )
         Group.objects.create(
-            group_id='first-group'
+            group_id='first_group'
         )
         Group.objects.create(
-            group_id='second-group'
+            group_id='second_group'
         )
         Category.objects.create(
-            cat_id='first-category',
-            group_id=Group.objects.get(group_id='first-group')
+            cat_id='first_category',
+            group_id=Group.objects.get(group_id='first_group')
         )
         Category.objects.create(
-            cat_id='second-category',
-            group_id=Group.objects.get(group_id='second-group')
+            cat_id='second_category',
+            group_id=Group.objects.get(group_id='second_group')
+        )
+        Subcategory.objects.create(
+            subcat_id='first_subcat',
+            cat_id=Category.objects.get(cat_id='first_category')
+        )
+        Subcategory.objects.create(
+            subcat_id='second_subcat',
+            cat_id=Category.objects.get(cat_id='second_category')
+        )
+        Product.objects.create(
+            pr_sku_id='first_product',
+            pr_subcat_id=Subcategory.objects.get(subcat_id='first_subcat'),
+            pr_uom_id=12
+        )
+        Product.objects.create(
+            pr_sku_id='second_product',
+            pr_subcat_id=Subcategory.objects.get(subcat_id='second_subcat'),
+            pr_uom_id=22
+        )
+        Shop.objects.create(
+            st_id='first_store',
+            st_city_id=City.objects.get(city_id='Moscow'),
+            st_division_code_id=Division.objects.get(division_code_id='first_division'),
+            st_type_format_id=28,
+            st_type_loc_id=56,
+            st_type_size_id=42,
+            st_is_active=True
+        )
+        Shop.objects.create(
+            st_id='second_store',
+            st_city_id=City.objects.get(city_id='Kemerovo'),
+            st_division_code_id=Division.objects.get(division_code_id='second_division'),
+            st_type_format_id=28,
+            st_type_loc_id=56,
+            st_type_size_id=42,
+            st_is_active=True
+        )
+        Sale.objects.create(
+            st_id=Shop.objects.get(st_id='first_store'),
+            pr_sku_id=Product.objects.get(pr_sku_id='first_product'),
+            date='2023-05-28',
+            pr_sales_type_id=True,
+            pr_sales_in_units=10,
+            pr_promo_sales_in_units=10,
+            pr_sales_in_rub=0,
+            pr_promo_sales_in_rub=1000
+        )
+        Sale.objects.create(
+            st_id=Shop.objects.get(st_id='first_store'),
+            pr_sku_id=Product.objects.get(pr_sku_id='first_product'),
+            date='2023-05-29',
+            pr_sales_type_id=False,
+            pr_sales_in_units=10,
+            pr_promo_sales_in_units=0,
+            pr_sales_in_rub=2000,
+            pr_promo_sales_in_rub=0
+        )
+        Sale.objects.create(
+            st_id=Shop.objects.get(st_id='second_store'),
+            pr_sku_id=Product.objects.get(pr_sku_id='second_product'),
+            date='2023-05-28',
+            pr_sales_type_id=True,
+            pr_sales_in_units=10,
+            pr_promo_sales_in_units=10,
+            pr_sales_in_rub=0,
+            pr_promo_sales_in_rub=1000
+        )
+        Sale.objects.create(
+            st_id=Shop.objects.get(st_id='second_store'),
+            pr_sku_id=Product.objects.get(pr_sku_id='second_product'),
+            date='2023-05-29',
+            pr_sales_type_id=False,
+            pr_sales_in_units=10,
+            pr_promo_sales_in_units=0,
+            pr_sales_in_rub=2000,
+            pr_promo_sales_in_rub=0
         )
 
     def setUp(self) -> None:
@@ -128,138 +205,126 @@ class ApiTest(APITestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(len(response.data), City.objects.count())
 
+    def test_api_v1_cities_id(self):
+        city = City.objects.first()
+        response = self.auth_client.get(
+            f'/api/v1/cities/{city.id}/'
+        )
+        self.assertEqual(response.data['city_id'], city.city_id)
+
     def test_api_v1_divisions(self):
         response = self.auth_client.get('/api/v1/divisions/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(len(response.data), Division.objects.count())
+
+    def test_api_v1_divisions_id(self):
+        div = Division.objects.first()
+        response = self.auth_client.get(f'/api/v1/divisions/{div.id}/')
+        self.assertEqual(
+            response.data['division_code_id'], div.division_code_id
+        )
 
     def test_api_v1_groups(self):
         response = self.auth_client.get('/api/v1/groups/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(len(response.data), Group.objects.count())
 
+    def test_api_v1_groups_id(self):
+        group = Group.objects.first()
+        response = self.auth_client.get(f'/api/v1/grops/{group.id}/')
+        self.assertEqual(response.data['group_id'], group.group_id)
+
     def test_api_v1_catrgories(self):
         response = self.auth_client.get('/api/v1/categories/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(len(response.data), Category.objects.count())
 
-    # def test_filter_group(self):
-    #     response = self.auth_client.get('/api/v1/filters/groups_whith_sales/')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(len(response.data), Shop.objects.count())
-    #     for shop in response.data:
-    #         print(shop)
-    #         with self.subTest(msg=f'{shop["id"]}', shop=shop):
-    #             for group in shop['groups']:
-    #                 print(group)
-    #                 with self.subTest(msg=f'{group["id"]}', group=group):
-    #                     self.assertEqual(
-    #                         (Product.objects.filter(
-    #                             sales__st_id=shop['id'],
-    #                             pr_subcat_id__cat_id__group_id=group['id']
-    #                         ).exists()), True
-    #                     )
+    def test_api_v1_categories_id(self):
+        cat = Category.objects.first()
+        response = self.auth_client.get(f'/api/v1/categories/{cat.id}/')
+        self.assertEqual(response.data['cat_id'], cat.cat_id)
+        self.assertEqual(response.data['group_id'], cat.group_id.id)
 
-    # def test_filter_category(self):
-    #     response = self.auth_client.get('/api/v1/filters/groups_whith_sales/')
-    #     for shop in response.data:
-    #         for group in shop['groups']:
-    #             response = self.auth_client.get(
-    #                 (f'/api/v1/filters/categories_with_sales/'
-    #                  f'?store={shop["id"]}&group={group["id"]}')
-    #             )
-    #             self.assertEqual(response.status_code, 200)
-    #             for category in response.data[0]['groups'][0]['categories']:
-    #                 with self.subTest(category=category):
-    #                     self.assertEqual(
-    #                         (Product.objects
-    #                          .filter(
-    #                             sales__pr_sales_type_id=False,
-    #                             sales__st_id=shop['id'],
-    #                             pr_subcat_id__cat_id__group_id=group['id'],
-    #                             pr_subcat_id__cat_id=category['id']
-    #                             ).exists()), True
-    #                     )
+    def test_api_v1_products(self):
+        response = self.auth_client.get('/api/v1/products/')
+        self.assertEqual(response.data['count'], Product.objects.count())
+        self.assertTrue(response.data['results'])
 
-    # def test_filter_category2(self):
-    #     response = self.auth_client.get('/api/v1/filters/groups_whith_sales/')
-    #     for shop in response.data:
-    #         for group in shop['groups']:
-    #             response = self.auth_client.get(
-    #                 (f'/api/v1/filters/category/'
-    #                  f'?store={shop["id"]}&group={group["id"]}')
-    #             )
-    #             self.assertEqual(response.status_code, 200)
-    #             for category in response.data:
-    #                 with self.subTest(category=category):
-    #                     self.assertEqual(
-    #                         (Product.objects.filter(
-    #                             sales__pr_sales_type_id=False,
-    #                             sales__st_id=shop['id'],
-    #                             pr_subcat_id__cat_id__group_id=group['id'],
-    #                             pr_subcat_id__cat_id=category['id']
-    #                             ).exists()), True
-    #                     )
+    def test_api_v1_products_id(self):
+        product = Product.objects.first()
+        response = self.auth_client.get(f'/api/v1/products/{product.id}/')
+        self.assertEqual(set(response.data.keys()), set(('id', 'sku')))
+        self.assertEqual(response.data['sku'], product.pr_sku_id)
 
-    # def test_filter_subcat(self):
-    #     response = self.auth_client.get('/api/v1/filters/groups_whith_sales/')
-    #     for shop in response.data:
-    #         for group in shop['groups']:
-    #             response = self.auth_client.get(
-    #                 (f'/api/v1/filters/category/'
-    #                  f'?store={shop["id"]}&group={group["id"]}')
-    #             )
-    #             for category in response.data:
-    #                 response = self.auth_client.get(
-    #                     (f'/api/v1/filters/subcategories_with_sales/'
-    #                      f'?store={shop["id"]}&group={group["id"]}'
-    #                      f'&category={category["id"]}')
-    #                 )
-    #                 for subcat in (
-    #                     response.data[0]['categories'][0]['subcategories']
-    #                 ):
-    #                     with self.subTest(subcat=subcat):
-    #                         self.assertTrue(
-    #                             (Product.objects.filter(
-    #                                 sales__pr_sales_type_id=False,
-    #                                 sales__st_id=shop['id'],
-    #                                 pr_subcat_id__cat_id__group_id=group['id'],
-    #                                 pr_subcat_id__cat_id=category['id'],
-    #                                 pr_subcat_id=subcat["id"]
-    #                             ).exists())
-    #                         )
+    def test_api_v1_shops(self):
+        response = self.auth_client.get('/api/v1/shops/')
+        self.assertEqual(len(response.data), Shop.objects.count())
+        self.assertEqual(
+            set(response.data[0].keys()),
+            set(
+                ('id', 'store', 'city', 'division', 'type_format', 'loc',
+                 'size', 'is_active')
+            )
+        )
 
-    # def test_filter_product(self):
-    #     response = self.auth_client.get('/api/v1/filters/groups_whith_sales/')
-    #     for shop in response.data:
-    #         for group in shop['groups']:
-    #             response = self.auth_client.get(
-    #                 (f'/api/v1/filters/category/'
-    #                  f'?store={shop["id"]}&group={group["id"]}')
-    #             )
-    #             for category in response.data:
-    #                 response = self.auth_client.get(
-    #                     (f'/api/v1/filters/subcategories_with_sales/'
-    #                      f'?store={shop["id"]}&group={group["id"]}'
-    #                      f'&category={category["id"]}')
-    #                 )
-    #                 for subcat in (
-    #                     response.data[0]['categories'][0]['subcategories']
-    #                 ):
-    #                     response = self.auth_client.get(
-    #                         (f'/api/v1/filters/products/'
-    #                          f'?store={shop["id"]}&group={group["id"]}'
-    #                          f'&category={category["id"]}'
-    #                          f'&subcategory={subcat["id"]}')
-    #                     )
-    #                     self.assertTrue(response.data)
+    def test_api_v1_shops_id(self):
+        shop = Shop.objects.first()
+        response = self.auth_client.get(f'/api/v1/shops/{shop.id}/')
+        self.assertEqual(response.data['store'], shop.st_id)
+        self.assertEqual(
+            set(response.data.keys()),
+            set(
+                ('id', 'store', 'city', 'division', 'type_format', 'loc',
+                 'size', 'is_active')
+            )
+        )
 
-    # def test_get_forecast(self):
-    #     response = self.auth_client.get(
-    #         '/api/v1/forecast/get_forecast/?store=12&product=1706'
-    #     )
-    #     self.assertTrue(response.data)
-    #     response = self.auth_client.get(
-    #         '/api/v1/forecast/get_forecast/?store=12&product=170'
-    #     )
-    #     self.assertFalse(response.data)
+    def test_api_v1_sales(self):
+        response = self.auth_client.get('/api/v1/sales/')
+        self.assertEqual(response.data['count'], Sale.objects.count())
+        self.assertEqual(
+            set(response.data['results'][0].keys()),
+            set(
+                ('st_id', 'pr_sku_id', 'date', 'pr_sales_in_units')
+            )
+        )
+
+    def test_api_v1_sales_id(self):
+        sale = Sale.objects.first()
+        response = self.auth_client.get(f'/api/v1/sales/{sale.id}/')
+        self.assertEqual(
+            set(response.data.keys()),
+            set(
+                ('st_id', 'pr_sku_id', 'date', 'pr_sales_in_units')
+            )
+        )
+        self.assertEqual(response.data['pr_sku_id'], sale.pr_sku_id.id)
+
+    def test_api_v1_filters_groups_whith_sales(self):
+        response = self.auth_client.get(
+            '/api/v1/filters/groups_whith_sales/'
+        )
+        self.assertEqual(len(response.data), Shop.objects.count())
+        self.assertEqual(
+            set(response.data[0].keys()),
+            set(
+                ('id', 'st_id', 'groups')
+            )
+        )
+        self.assertEqual(
+            set(response.data[0]['groups'][0].keys()),
+            set(('id', 'group_id'))
+        )
+        self.assertTrue(Sale.objects.filter(
+            st_id=get_object_or_404(Shop, id=response.data[0]['id']),
+            pr_sku_id=get_object_or_404(
+                Product,
+                pr_subcat_id__cat_id__group_id=response.data[0]['groups'][0]['id']
+            )
+        ).exists())
+
+    def test_api_v1_filters_categories_with_sales(self):
+        response = self.auth_client.get(
+            '/api/v1/filters/categories_with_sales/?store=1&group=1'
+        )
+        self.assertEqual(response.status_code, HTTPStatus.OK)
